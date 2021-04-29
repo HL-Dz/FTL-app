f6054abe8f8313ee759ee7c6a20f66c36f494064
Dzmitry Hlushak <hlushak.dzmitry@gmail.com>
2021-04-29 23:27:11 +0300

    feat: add background and players to Epl component

--- src/components/pages/Leagues/EPL/Epl.jsx
+++ src/components/pages/Leagues/EPL/Epl.jsx
@@ -3,10 +3,28 @@ import LeagueTemplate from '../../../common/LeagueTemplate/LeagueTemplate';
 import Preloader from '../../../common/Preloader/Preloader';
 import "./Epl.scss";
 
+import emirates from "./Emirates.jpg";
+
+
+let eplStyles = {
+  backgroundImage: `url(${emirates})`,
+  backgroundPosition: "top center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "contain"
+}
+
 
 const Epl = ({league, isFetching}) => {
   return (
-    <div className="epl">
+    <div className="epl" style={eplStyles}>
+        <div className="players">
+          <div className="player">
+            <img src="./images/Epl/Leno.png" alt="Leno"/>
+          </div>
+          <div className="player">
+            <img src="./images/Epl/Pogba.png" alt="Pogba"/>
+          </div>
+        </div>
       <div className="container">
         {isFetching ? <Preloader/> : <LeagueTemplate league={league}/>}
       </div>
